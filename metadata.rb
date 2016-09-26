d52ad7caa663d366f07416bbf559bd1ab113d800
Matt Wrock <matt@mattwrock.com>
2016-09-25 22:52:29 -0700

    add license

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,7 @@
 name 'docker_nano_host'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures docker_nano_host'
-long_description 'Installs/Configures docker_nano_host'
+maintainer 'Matt Wrock'
+maintainer_email 'matt@mattwrock.com'
+license 'Apache 2.0'
+description 'Installs/Configures docker and configures to run as a host'
+long_description 'Installs/Configures docker and configures to run as a host'
 version '0.1.0'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/docker_nano_host/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/docker_nano_host' if respond_to?(:source_url)
