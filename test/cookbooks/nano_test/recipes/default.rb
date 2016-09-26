dcd915274bb3567f1e9ff093bd70d8958bb1689a
Matt Wrock <matt@mattwrock.com>
2016-09-26 00:51:15 -0700

    add a commnt about tk bug and workaround recipe

--- test/cookbooks/nano_test/recipes/default.rb
+++ test/cookbooks/nano_test/recipes/default.rb
@@ -1,2 +1,4 @@
+# There is a bug in Test-Kitchen on nano that clears the PATH
+# This will be fixed in the next release but is fixed by this:
 ENV['PATH'] += ";C:\\Windows\\system32;C:\\Windows;C:\\Windows\\System32\\Wbem;C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\;C:\\Users\\vagrant\\AppData\\Local\\Microsoft\\WindowsApps"
 node.automatic[:languages][:powershell][:version] = "5.1.14368.1000"
