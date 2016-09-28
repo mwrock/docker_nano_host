8578023bf81c5803a6926544017dce7024f9f944
Matt Wrock <matt@mattwrock.com>
2016-09-28 16:46:13 -0700

    remove hack now that test-kitchen 1.13.2 is out

--- Berksfile
+++ Berksfile
@@ -1,7 +1,3 @@
 source 'https://supermarket.chef.io'
 
 metadata
-
-group :integration do
-  cookbook 'nano_test', path: './test/cookbooks/nano_test'
-end
--- test/cookbooks/nano_test/metadata.rb
+++
@@ -1,6 +0,0 @@
-name 'nano_test'
-maintainer 'Maintainer'
-maintainer_email 'Maintainer@example.com'
-license 'Apache 2.0'
-description 'fixes test-kitchen runs on nano in 1.13.1 until the next release'
-version '0.1.0'
